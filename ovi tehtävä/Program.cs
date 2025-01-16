1a29b80f98e3c2c7c7a2403646db535817817fa1
Aliaa1406 <s2300914@edu.bc.fi>
2025-01-16 12:10:36 +0200

    ovi tehtävä

---
+++ ovi tehtävä/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ovi_tehtävä
+{
+    internal class Program
+    {
+        enum PlayerDoor
+        {
+            Locked,
+            close,
+            open,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PlayerDoor state = PlayerDoor.Locked;
+
+            while (true)
+            {
+                Console.WriteLine($"The Door is {state}. what do you want to do ?");
+                string choose = Console.ReadLine()?.ToLower();
+
+
+
+                if (state == PlayerDoor.Locked)
+                {
+                    if (choose == "poista lukitus")
+                    {
+                        state = PlayerDoor.close;
+                    }
+                }
+                else if (state == PlayerDoor.close)
+                {
+                    if (choose == "open")
+                    {
+                        state = PlayerDoor.open;
+                    }
+                    else if(choose == "lock it")
+                    {
+                        state = PlayerDoor.Locked;
+                    }
+                }
+                else if (state == PlayerDoor.open)
+                {
+                    if (choose == "close")
+                    {
+                        state = PlayerDoor.close;
+                    }
+                }
+                
+
+
+
+            }
+        }
+    }
+}
+
