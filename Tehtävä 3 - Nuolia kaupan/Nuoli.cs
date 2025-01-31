4bb6759881d1fa916000c637c4a53ceb374c15a5
Aliaa1406 <s2300914@edu.bc.fi>
2025-01-31 09:36:30 +0200

    nuoli tehtävä 1

---
+++ Tehtävä 3 - Nuolia kaupan/Nuoli.cs
@@ -0,0 +1,62 @@
+﻿namespace Tehtävä_3___Nuolia_kaupan
+{
+
+
+    enum KärkiMateriaali
+    {
+        Puu,
+        Teräs,
+        Timantti,
+        Kivi,
+    }
+
+    enum SulkeMateriaali
+    {
+        Lehti,
+        Kanansulka,
+        Kotkanasulka,
+
+    }
+
+    internal class Nuoli
+    {
+        sbyte pituusCm;
+        SulkeMateriaali sulka;
+        KärkiMateriaali Kärki;
+
+        // tee konstruktori
+        public Nuoli(KärkiMateriaali kärki, SulkeMateriaali sulke, float pituusCm)
+        {
+
+            this.Kärki = kärki;
+            this.sulka = sulke;
+            this.pituusCm = (sbyte)pituusCm;
+
+        }
+        // prise account 
+        public float AnnaHinta()
+        {
+            float hinta = 0;
+            hinta += Kärki switch
+            {
+                KärkiMateriaali.Puu => 3,
+                KärkiMateriaali.Teräs => 5,
+                KärkiMateriaali.Timantti => 50,
+            };
+            hinta += sulka switch
+            {
+                SulkeMateriaali.Lehti => 0,
+                SulkeMateriaali.Kotkanasulka => 1,
+                SulkeMateriaali.Kanansulka => 5,
+            };
+            
+            hinta += pituusCm * 0.05f;
+
+            return hinta;
+        }
+
+    }
+
+}
+
+
---
+++ Tehtävä 3 - Nuolia kaupan/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Tehtävä_3___Nuolia_kaupan
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int Hinta = 0;
+
+            KärkiMateriaali valittuKärki = KärkiMateriaali.Puu;
+            SulkeMateriaali valittuSukla = SulkeMateriaali.Lehti;
+            sbyte pituusCm = 0;
+            while (true)
+            {
+                Console.WriteLine("Valitse kärki materiaali \nPuu\nTeräs\nTimantti");
+                string? chooser = Console.ReadLine();
+                if (chooser.ToLower() == "puu")
+                {
+                    valittuKärki = KärkiMateriaali.Puu;
+                    break;
+                }
+                else if (chooser.ToLower() == "Timantti")
+                {
+                    valittuKärki = KärkiMateriaali.Timantti;
+                    break;
+                }
+                else if (chooser.ToLower() == "Teräs")
+                {
+                    valittuKärki = KärkiMateriaali.Teräs;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect choice");
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Opstin sulke Maeriaal: \n  Lehti\n  Kanansulka\n   Kotkanasulka " );
+                string? chooser = Console.ReadLine().ToLower();
+                if (chooser == "lehti")
+                {
+                    valittuSukla = SulkeMateriaali.Lehti;
+                    break;
+
+                }
+                else if (chooser == "Kanansulka")
+                {
+                    valittuSukla = SulkeMateriaali.Kanansulka;
+                    break;
+                }
+                else if (chooser == "Kotkanasulka")
+                {
+                    valittuSukla = SulkeMateriaali.Kotkanasulka;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect choice");
+                    
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("anna pituus nuola (60-100 CM): ");
+
+                try
+                {
+                    pituusCm = sbyte.Parse(Console.ReadLine().ToLower());
+                    if (pituusCm >= 60 && pituusCm <= 100)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("pituuus on vällllä 60-100 Cm.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Syotä kwlvollinen numero.");
+                }
+            }
+
+            Nuoli nuoli = new Nuoli(valittuKärki, valittuSukla, pituusCm);
+            float hinta = nuoli.AnnaHinta();
+
+            Console.WriteLine($"Nuola hinta on:{hinta}€ kultaa");
+
+
+
+            //kysy nuolen kärki, sukla ja pituus
+            // kysy pituua uudentaan jos se on <60|| >100
+
+            //Tulosta nuolen hinta
+        }
+    }
+}
