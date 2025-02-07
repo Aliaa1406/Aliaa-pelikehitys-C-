37472443869f53a0fb6864894a8f4cd230aa4e3d
Aliaa1406 <s2300914@edu.bc.fi>
2025-02-07 09:23:50 +0200

    Törmäykset tehtävä

---
+++ Törmäykset/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Raylib_cs;
+namespace Törmäykset
+{
+
+    public class CollisionHandler
+    {
+        public static void HandlePointRectangleCollision( ref Vector2 position, ref Vector2  )
+    } 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
