5ebfd5acf7448860eba13014a8911cfc34704d58
Aliaa1406 <s2300914@edu.bc.fi>
2025-09-02 09:19:10 +0300

    Asteroite
    update

--- Space Shooter/AsteroidsGame.cs
+++ Space Shooter/AsteroidsGame.cs
@@ -342,24 +342,45 @@ namespace Space_Shooter
             asteroids.Add(new Asteroid(position, velocity, size, rotationSpeed));
         }
 
+        // طريقة مبسطة لاختيار موقع عشوائي للظهور
         private Vector2 GetRandomSpawnPosition()
         {
+            // اختيار جانب عشوائي من الشاشة (أعلى، يمين، أسفل، يسار)
             int side = random.Next(4);
-            return side switch
+
+            Vector2 spawnPosition;
+
+            if (side == 0) // أعلى الشاشة
             {
-                0 => new Vector2(random.Next(SCREEN_WIDTH), -50), // Top
-                1 => new Vector2(SCREEN_WIDTH + 50, random.Next(SCREEN_HEIGHT)), // Right
-                2 => new Vector2(random.Next(SCREEN_WIDTH), SCREEN_HEIGHT + 50), // Bottom
-                _ => new Vector2(-50, random.Next(SCREEN_HEIGHT)) // Left
-            };
+                spawnPosition = new Vector2(random.Next(SCREEN_WIDTH), -50);
+            }
+            else if (side == 1) // يمين الشاشة
+            {
+                spawnPosition = new Vector2(SCREEN_WIDTH + 50, random.Next(SCREEN_HEIGHT));
+            }
+            else if (side == 2) // أسفل الشاشة
+            {
+                spawnPosition = new Vector2(random.Next(SCREEN_WIDTH), SCREEN_HEIGHT + 50);
+            }
+            else // يسار الشاشة
+            {
+                spawnPosition = new Vector2(-50, random.Next(SCREEN_HEIGHT));
+            }
+
+            return spawnPosition;
         }
 
+        // طريقة مبسطة لحساب الاتجاه نحو وسط الشاشة
         private Vector2 GetDirectionToCenter(Vector2 fromPosition)
         {
+            // حساب الفرق بين الموقع الحالي ووسط الشاشة
             Vector2 direction = ScreenCenter - fromPosition;
-            return Vector2.Normalize(direction); // Using Vector2.Normalize instead of manual calculation
+
+            // تطبيع الاتجاه (جعل طوله = 1) باستخدام دالة جاهزة
+            return Vector2.Normalize(direction);
         }
 
+        // طريقة مبسطة لتقسيم الكويكبات - بدون استخدام Sin/Cos معقد
         private void SplitAsteroid(Asteroid asteroid)
         {
             score += asteroid.GetSize() * 100;
@@ -368,19 +389,28 @@ namespace Space_Shooter
             if (Raylib.IsSoundValid(ExplosionSound))
                 Raylib.PlaySound(ExplosionSound);
 
-            // Only big asteroids (size 2) split into small asteroids (size 1)
+            // فقط الكويكبات الكبيرة (حجم 2) تنقسم إلى صغيرة (حجم 1)
             if (asteroid.GetSize() == 2)
             {
-                // Create two small asteroids
-                for (int i = 0; i < 2; i++)
-                {
-                    float angle = (float)random.NextDouble() * MathF.PI * 2;
-                    Vector2 direction = new Vector2(MathF.Cos(angle), MathF.Sin(angle));
-                    Vector2 velocity = direction * (70 + (float)random.NextDouble() * 30);
-                    float rotationSpeed = (float)(random.NextDouble() * 200 - 100);
+                // إنشاء كويكبين صغيرين بطريقة مبسطة
+                Vector2 currentPos = asteroid.GetPosition();
 
-                    asteroids.Add(new Asteroid(asteroid.GetPosition(), velocity, 1, rotationSpeed)); // size 1 = small
-                }
+                // الكويكب الأول: يتحرك يميناً وأعلى
+                Vector2 direction1 = new Vector2(1, -0.5f); // يمين + قليل أعلى
+                direction1 = Vector2.Normalize(direction1);
+                Vector2 velocity1 = direction1 * (70 + (float)random.NextDouble() * 30);
+
+                // الكويكب الثاني: يتحرك يساراً وأسفل
+                Vector2 direction2 = new Vector2(-1, 0.5f); // يسار + قليل أسفل
+                direction2 = Vector2.Normalize(direction2);
+                Vector2 velocity2 = direction2 * (70 + (float)random.NextDouble() * 30);
+
+                // سرعة دوران عشوائية
+                float rotationSpeed1 = (float)(random.NextDouble() * 200 - 100);
+                float rotationSpeed2 = (float)(random.NextDouble() * 200 - 100);
+
+                asteroids.Add(new Asteroid(currentPos, velocity1, 1, rotationSpeed1));
+                asteroids.Add(new Asteroid(currentPos, velocity2, 1, rotationSpeed2));
             }
 
             asteroid.Destroy();
@@ -540,7 +570,7 @@ namespace Space_Shooter
             {
                 2 => ASTEROID_BROWNTexture,
                 1 => ASTEROID_GREYTexture,
-                
+                _ => ASTEROID_BROWNTexture
             };
         }
 
@@ -553,5 +583,27 @@ namespace Space_Shooter
         public static int GetScreenWidth() => SCREEN_WIDTH;
         public static int GetScreenHeight() => SCREEN_HEIGHT;
         public static Vector2 GetScreenCenter() => ScreenCenter;
+
+        // دوال مساعدة مبسطة لأي استخدام مستقبلي
+        public static Vector2 CreateDirectionFromAngle(float angleDegrees)
+        {
+            // تحويل الزاوية من درجات إلى راديان
+            float angleRadians = angleDegrees * MathF.PI / 180f;
+
+            // إنشاء اتجاه باستخدام الدوال المثلثية البسيطة
+            return new Vector2(MathF.Cos(angleRadians), MathF.Sin(angleRadians));
+        }
+
+        public static float GetAngleBetweenPoints(Vector2 from, Vector2 to)
+        {
+            Vector2 direction = to - from;
+            float angleRadians = MathF.Atan2(direction.Y, direction.X);
+            float angleDegrees = angleRadians * 180f / MathF.PI;
+
+            // التأكد من أن الزاوية بين 0 و 360
+            if (angleDegrees < 0) angleDegrees += 360;
+
+            return angleDegrees;
+        }
     }
 }
